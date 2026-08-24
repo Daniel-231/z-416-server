aaac7191dc7cff423bb80704d64feb9a8544b285
Daniel-231 <daniel20040426@gmail.com>
2026-08-24 13:44:08 +0300

    Added authentication fields to User model

---
+++ prisma/migrations/20260824104348_user_auth_fields/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `passwordHash` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[authId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `authId` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `authProvider` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "AuthProvider" AS ENUM ('GOOGLE', 'APPLE');
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "passwordHash",
+ADD COLUMN     "authId" TEXT NOT NULL,
+ADD COLUMN     "authProvider" "AuthProvider" NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_authId_key" ON "User"("authId");
