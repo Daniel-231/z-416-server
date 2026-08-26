fbed98926aa21d05c48187929a895729ce92b36c
Daniel-231 <daniel20040426@gmail.com>
2026-08-26 21:29:07 +0300

    Add authentication middleware to /all_users route

--- src/Routes/usersRouter.ts
+++ src/Routes/usersRouter.ts
@@ -1,9 +1,10 @@
 import { type Request, type Response, Router } from "express";
+import { requireAuth }from "../Middleware/requireAuth";
 import { prisma } from "../lib/prisma";
 
 const router = Router();
 
-router.get("/all_users", async (_req: Request, res: Response) => {
+router.get("/all_users", requireAuth, async (_req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany();
     res.json({"users": users});
@@ -11,6 +12,6 @@ router.get("/all_users", async (_req: Request, res: Response) => {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch users" });
   }
-}); 
+});
 
 export default router;
